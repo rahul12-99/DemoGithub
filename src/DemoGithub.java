68917770e4f73822de405012d5655706ba79b117
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-02-21 14:38:36 +0530

    Added: Addition of two number

--- src/DemoGithub.java
+++ src/DemoGithub.java
@@ -1,5 +1,10 @@
 public class DemoGithub {
     public static void main(String[] args) {
+
+        int x = 10;
+        int y = 5;
+        int z = x + y;
+        System.out.println(z);
         System.out.println("welcome To Github");
     }
 }
